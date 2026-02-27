f601892f0366c6775f9cd243470333e867cfd872
Bita Yeganeh <b.yeganeh85@gmail.com>
2026-02-27 23:16:46 +0200

    wip

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// --- Favicon setup ---
+import iconUrl from './assets/icon.png' // Vite will handle hashing in production
+
+const link = document.createElement('link')
+link.rel = 'icon'
+link.href = iconUrl
+document.head.appendChild(link)
+
+// --- React render ---
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
